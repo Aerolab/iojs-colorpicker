46c64c39ad4c9aae7152ff3dcf0c0e0833eac89b
Roberto González <roberto@aerolab.com.ar>
2014-12-16 01:50:28 -0300

    Added "permalinks" (mostly untested)

--- js/scripts.js
+++ js/scripts.js
@@ -13,8 +13,16 @@ var background = Color('#032c2c');
 var defaultShadowOpacity = 0.3;
 
 var currentColor = 'f26b24';
+var currentBackground = '032c2c';
 var currentOpacity = defaultShadowOpacity;
 
+// Restore the defaults if they are present in the hash
+var chunks = window.location.hash.split('-');
+if( chunks.length === 3 ) {
+  currentColor = chunks[0];
+  currentBackground = chunks[1];
+  currentOpacity = chunks[2];
+}
 
 function updateLogo(newColor, shadowOpacity) {
   var color = Color('#'+newColor);
@@ -42,6 +50,8 @@ function updateLogo(newColor, shadowOpacity) {
 $(document).ready(function(){
 
 
+  var hashChangeTimeout = null;
+
   $('#main_color').colpick({
     layout:'hex',
     submit:0,
@@ -52,10 +62,15 @@ $(document).ready(function(){
 
       currentColor = hex;
       updateLogo(currentColor, currentOpacity);
+
+      clearTimeout(hashChangeTimeout);
+      hashChangeTimeout = setTimeout(function(){
+        window.location.hash = currentColor +'-'+ currentBackground +'-'+ Math.round(currentOpacity*100)/100;
+      }, 300);
     }
   }).keyup(function(){
     $(this).colpickSetColor(this.value);
-  }).colpickSetColor( primary.toCSSHex() );
+  }).colpickSetColor( currentColor );
 
 
   $('#background_color').colpick({
@@ -66,29 +81,40 @@ $(document).ready(function(){
       // Fill the text box just if the color was set using the picker, and not the colpickSetColor function.
       if(!bySetColor) $(el).val(hex);
 
-      $('main').css('background-color', '#'+hex);
+      currentBackground = hex;
+      $('main').css('background-color', '#'+currentBackground);
+
+      clearTimeout(hashChangeTimeout);
+      hashChangeTimeout = setTimeout(function(){
+        window.location.hash = currentColor +'-'+ currentBackground +'-'+ Math.round(currentOpacity*100)/100;
+      }, 300);
     }
   }).keyup(function(){
     $(this).colpickSetColor(this.value);
-  }).colpickSetColor( background.toCSSHex() );
+  }).colpickSetColor( currentBackground );
 
 
   $("#shadow_opacity").bind("slider:changed", function (event, data) {
     currentOpacity = data.ratio;
     updateLogo(currentColor, currentOpacity);
-  }).simpleSlider().simpleSlider('setRatio', defaultShadowOpacity);
+
+    clearTimeout(hashChangeTimeout);
+    hashChangeTimeout = setTimeout(function(){
+      window.location.hash = currentColor +'-'+ currentBackground +'-'+ Math.round(currentOpacity*100)/100;
+    }, 300);
+  }).simpleSlider().simpleSlider('setRatio', currentOpacity);
 
 
 
   $('#randomize').on('click', function(event){
     event.preventDefault();
     var newColor = Color('#ffffff')
-      .setHue( Math.random() * 240 )
+      .setHue( Math.random() * 360 )
       .setValue( 0.8 + Math.random() * 0.2 )
       .setSaturation( 0.7 + Math.random() * 0.3 );
 
     $('#main_color').colpickSetColor( newColor.toCSSHex() );
-    $('#background_color').colpickSetColor( newColor.triadicScheme()[ Math.floor(Math.random() * 2) + 1 ].toCSSHex() );
+    $('#background_color').colpickSetColor( newColor.shiftHue( (Math.random() > 0.5 ? +1 : -1) * 60).toCSSHex() );
 
     $("#shadow_opacity").simpleSlider('setRatio', defaultShadowOpacity);
   });
@@ -102,5 +128,8 @@ $(document).ready(function(){
 
     $("#shadow_opacity").simpleSlider('setRatio', defaultShadowOpacity);
   });
+
+
+  $('main').removeClass('loading');
 
 });
